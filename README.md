6dea25a98d97f7489d419d8b057286582ac2893b
steam <jizhengqistrongman@126.com>
2020-06-17 15:41:36 +0800

    some simple fix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # babelPlugin
 babel plugin test
+
+## @babel/parser, @babel/traverse
+```javascript
+import { parse } from '@babel/parser';
+import traverse from '@babel/traverse';
+```
+parse 就是把 code 转成 AST。
+traverse 遍历 ast，然后对 ast 改造。
+
+感觉 traverse 好像可以替代 babel plugins ？ 
--- plugins/transformObjProp.js
+++ plugins/transformObjProp.js
@@ -1,12 +1,12 @@
-// const inspect = require('util').inspect;
-// const fs = require('fs');
+const inspect = require('util').inspect;
+const fs = require('fs');
 
 function transformObjPropPlugin({types: t}) {
   return {
     visitor: {
       MemberExpression(path) {
         const { node, scope } = path;
-        // fs.writeFile('./output.js', inspect(scope), 'utf8', err => console.log(err))
+        fs.writeFile('./scope.js', inspect(scope), 'utf8', err => console.log(err))
         if(t.isStringLiteral(node.property)) {
           node.property = t.identifier(node.property.value)
           node.computed = false; // false 时转成 obj.prop，否则会转换成 obj[prop]，
